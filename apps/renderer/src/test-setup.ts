db27394f57b903fc05ae1d0de3573ff9723ad825
Danny McGee <dannymcgee@gmail.com>
2021-05-08 19:05:55 -0400

    Fix failing test (again...?)

--- apps/renderer/src/test-setup.ts
+++ apps/renderer/src/test-setup.ts
@@ -1,9 +1,18 @@
 import "jest-preset-angular";
 
-Object.defineProperty(window, "electron", {
-	value: {
-		getAppVersion: async () => "",
-		send: async (_: string) => "",
-		platform: "Jest",
+declare global {
+	var electron: {
+		getAppVersion(): Promise<string>,
+		send(message: string): Promise<string>,
+		platform: string;
 	}
-})
+}
+
+const electron: PropertyDescriptor["value"] = {
+	getAppVersion: async () => "",
+	send: async (_: string) => "",
+	platform: "Jest",
+}
+
+Object.defineProperty(global, "electron", { value: electron });
+Object.defineProperty(window, "electron", { value: electron });
